b0d7eb0cd9415c977b71727dd5cba9a5d042fca5
John Stowers <john.stowers@gmail.com>
2009-02-27 20:03:32 +1300

    add build script that reconfigures and builds a deb package

--- build.sh
+++ build.sh
@@ -1,1 +1,19 @@
-./configure --disable-apps --with-python=no --with-swig=no --with-octave=no --with-unicap=no --with-v4l=yes --with-quicktime=no --with-ffmpeg=yes --with-gstreamer=no  --with-xine=no --with-gtk=yes --with-gthread=yes --with-1394libs=no --prefix=/usr/local 
+#!/bin/sh
+./configure 			\
+	--disable-apps		\
+	--with-python=no 	\
+	--with-swig=no 		\
+	--with-octave=no 	\
+	--with-unicap=no 	\
+	--with-v4l=yes 		\
+	--with-quicktime=no	\
+	--with-ffmpeg=yes	\
+	--with-gstreamer=no	\
+	--with-xine=no		\
+	--with-gtk=yes		\
+	--with-gthread=yes	\
+	--with-1394libs=no	\
+	--prefix=/usr/local \
+&& checkinstall 		\
+	-y -D				\
+	--install=no
