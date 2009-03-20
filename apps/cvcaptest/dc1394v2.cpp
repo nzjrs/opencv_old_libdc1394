a31b0e141042bdae131b30a717b8da19a121c5fc
John Stowers <john.stowers@gmail.com>
2009-03-20 17:21:54 +1300

    show video

--- apps/cvcaptest/dc1394v2.cpp
+++ apps/cvcaptest/dc1394v2.cpp
@@ -1,9 +1,36 @@
 #include "cv.h"
 #include "highgui.h"
 
+#include <stdio.h>
+
 int main( int argc, char* argv[] )
 {
-    CvCapture *a = cvCreateCameraCapture(300);
+    CvCapture* capture = NULL;
+    IplImage *frame = NULL;
+
+    capture = cvCreateCameraCapture(300);
+
+    if( capture )
+    {
+        cvNamedWindow( "result", CV_WINDOW_AUTOSIZE );
+
+        for(;;)
+        {
+            frame = cvQueryFrame( capture );
+            if( !frame ) {
+                printf("sadf\n");
+                break;
+            }
+
+            cvShowImage( "result", frame );
+
+            if( cvWaitKey( 10 ) >= 0 )
+                break;
+        }
+        cvReleaseCapture( &capture );
+        cvDestroyWindow("result");
+    }
+    
     return 0;
 }
 
