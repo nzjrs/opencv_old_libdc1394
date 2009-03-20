73017769e918a040a42259487888c329afd76730
John Stowers <john.stowers@gmail.com>
2009-03-20 17:49:52 +1300

    Generalise to support all CvCap backends

--- apps/cvcaptest/cvcap.cpp
+++ apps/cvcaptest/cvcap.cpp
@@ -1,14 +1,42 @@
 #include "cv.h"
 #include "highgui.h"
 
+#include <stdlib.h>
 #include <stdio.h>
+
+static void usage(char *me)
+{
+    printf(
+        "Usage:\n%s\t<base+index>\n"
+        "Base:\n"
+        "0\tAutodetect\n"
+        "100\tMIL proprietary drivers\n"
+        "200\tPlatform native (VFW, VFL2, etc)\n"
+        "300\tIEEE 1394 drivers\n"
+        "400\tTYZX proprietary drivers\n"
+        "500\tQuickTime\n"
+        "600\tUnicap drivers\n"
+        "700\tDirectShow (via videoInput)\n", me);
+}
 
 int main( int argc, char* argv[] )
 {
     CvCapture* capture = NULL;
     IplImage *frame = NULL;
+    int index = -1;
 
-    capture = cvCreateCameraCapture(300);
+    if (argc != 2) {
+        usage(argv[0]);
+        exit(1);
+    }
+
+    index = strtol(argv[1], NULL, 0);
+    if (index < 0 || index > 800) {
+        usage(argv[0]);
+        exit(2);
+    }        
+
+    capture = cvCreateCameraCapture(index);
 
     if( capture )
     {
